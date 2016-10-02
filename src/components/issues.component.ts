15be85d88d321a03153825f23fdb2883570e4fba
sisisin <azsisisin@gmail.com>
2016-10-02 23:51:43 +0900

    feat(issues) reload button.

--- src/components/issues.component.ts
+++ src/components/issues.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Issue, Label } from '../app.state';
+import { AppActions } from '../app.actions';
+import { AppDispatcher } from '../app.dispatcher';
 
 @Component({
   selector: 'issues',
   template: `
     <h1>your issues and prs</h1>
+    <input type="button" value="reload" (click)="onReload()" />
     <div *ngFor="let issue of issues">
       <span>{{head(issue)}}</span>
       <span *ngFor="let label of issue.labels">
@@ -17,9 +20,14 @@ import { Issue, Label } from '../app.state';
 export class Issues {
   @Input() issues: Issue[];
   constructor(
+    private actions: AppActions,
+    private dispatcher: AppDispatcher
   ) { }
   head(issue: Issue): string {
     const kind = issue.pull_request ? 'PR' : 'issue';
     return `[${issue.repositoryName} ${kind}]`;
+  }
+  onReload() {
+    this.dispatcher.emit(this.actions.fetchIssues());
   }
 }
